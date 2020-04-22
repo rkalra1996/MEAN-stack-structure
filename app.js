1170b700ce844d86c2a34104da3532428bf1cf99
rishabh kalra <rishabh.kalra@niit.com>
2020-04-22 12:06:19 +0530

    removed useless comments

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 // import all the dependencies
 const dependencies = require('./dependencies/dependedncies');
 const routes = require('./routes/routes');
-
 const app = dependencies.app;
 
-
-
 app.use('/api',routes)
-// app.use('/health', routes.healthRouter);
 
-/* app.use('/users', dependencies.auth.keycloak.protect(), (req, res, next) => {
-    // logger.info(JSON.stringify(req.kauth.grant.access_token.content));
-    next();
-}, routes.usersRouter);
- */
 module.exports = app;
