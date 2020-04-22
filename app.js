5eaebfa8ff0cbf1a4957ac8ac6e7b11d264718b6
rishabh kalra <rishabh.kalra@niit.com>
2020-04-22 10:32:20 +0530

    removed indexRouter, no longer needed

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // import all the dependencies
 const dependencies = require('./dependencies/dependedncies');
 const routes = require('./routes/routes');
-const logger = require('./middlewares/middlewares').logger.default;
 
 const app = dependencies.app;
 
@@ -9,18 +8,8 @@ const app = dependencies.app;
 app.use('/health', routes.healthRouter);
 
 app.use('/users', dependencies.auth.keycloak.protect(), (req, res, next) => {
-    logger.info('i am here users')
     // logger.info(JSON.stringify(req.kauth.grant.access_token.content));
     next();
 }, routes.usersRouter);
-
-app.use('/',(req, res, next) => {
-    logger.info('i am here')
-    // logger.info(JSON.stringify(req.kauth.grant.access_token.content));
-    next();
-},  routes.indexRouter);
-/* app.use(dependencies.auth.keycloak.middleware({logout: '/logout'}), (req,res,next) => {
-    logger.info('clicked on logout');
-}); */
 
 module.exports = app;
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,7 @@
-const indexRouter = require('./index');
 const usersRouter = require('./users');
 const healthRouter = require('./health');
 
 module.exports = {
-    indexRouter,
     usersRouter,
     healthRouter,
 }
