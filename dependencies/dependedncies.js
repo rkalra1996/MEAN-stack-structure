9b4086bbcbacbfe03d59e32408a16caf02452c6c
rishabh kalra <rishabh.kalra@niit.com>
2020-04-23 12:26:42 +0530

    minor changes

--- dependencies/dependedncies.js
+++ dependencies/dependedncies.js
@@ -1,17 +1,11 @@
 const path = require('path');
-
 const {httpProxyReqPathResolver, uplinkServerAddress} = require('./utils/reverse-proxy-utils');
-
 const httpLogger = require('./../middlewares/middlewares').logger.winstonHttpLogger;
-const {
-    compression, express,
-    cookieParser, auth, httpProxy} = require('./../middlewares/middlewares');
-
-// import auth module to setup authentication on routes
+const {compression, express,cookieParser, auth, httpProxy} = require('./../middlewares/middlewares');
+// setup authentication on routes
 const authenticator = auth.keycloak();
 // setup express dependencies
 const app = express();
-
 // setup initials
 app.use(compression());
 app.use(httpLogger);
@@ -19,11 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, './../public')));
-
 // setup authentication
 app.use(authenticator.expressSession(authenticator.sessionConfig));
 app.use(authenticator.keycloak.middleware());
-
 // use reverse proxy
 app.use(httpProxy(uplinkServerAddress, true, httpProxyReqPathResolver));
 
